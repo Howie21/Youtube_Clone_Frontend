cfa5edf045584c01d0ffdf0503039fc69e90d0b4
Howie21 <bryce9643@gmail.com>
2021-11-30 15:05:39 -0500

    Added SearchBar Display and logic to handle Searches.

---
+++ youtubeclone_frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            search: ''
+         }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        console.log(this.state.search)
+        this.props.startSearch(this.state.search)
+        this.setState({
+            search: ''
+        })
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <input value={this.state.search} name="search" onChange={this.handleChange}></input>
+                <button onClick={() => this.handleSubmit(this.state.search)}></button>
+            </div>
+         );
+    }
+}
+ 
+export default SearchBar;
