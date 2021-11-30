f85d8a1f0535746893cc3bac92f07f33271b9213
Howie21 <bryce9643@gmail.com>
2021-11-30 14:03:21 -0500

    Addition of logic to Display comments with the videoID of the loaded video

---
+++ youtubeclone_frontend/src/components/CommentsDisplay/CommentsDisplay.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Reply from '../Reply/Reply';
+
+const CommentsDisplay = (props) => {
+    const[comment,SetComment] = useState([])
+    const[toggle, SetToggle] = useState()
+    console.log(props)
+
+    useEffect(()=>{
+        
+    },[props.videoId, toggle])
+
+    const increment = async(id, name) => {
+        let newArr = props.comments
+        let value;
+        newArr.forEach(comment => {
+            if(comment.id === id) {
+                comment[name] += 1
+                value = comment[name]
+            }
+        })
+        await axios.patch(`http://127.0.0.1:8000/comment/${id}/`, { [name]: value })
+        SetToggle(!toggle)
+        SetComment(newArr)
+ }
+
+    return(
+        <div>
+            {props.comments.map(comment => {
+                        if(comment.videoId === props.videoId){ 
+                            return <div key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <button name="likes" onClick={() => increment(comment.id, 'likes')}>Like</button>
+                                     {<p>{comment.likes}</p>}
+                                    <button name="dislikes" onClick={() => increment(comment.id, 'dislikes')}>Dislike</button>
+                                    {<p>{comment.dislikes}</p>}
+                                    <Reply key={comment.id} commentId={comment.id} videoId={props.videoId} />
+                                    </div>
+                        }
+                    })}
+        </div>
+    )
+
+}
+
+export default CommentsDisplay
