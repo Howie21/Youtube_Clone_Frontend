a92cb507a02c712ff45594ad33f8616957ea4bbc
Howie21 <bryce9643@gmail.com>
2021-11-30 14:42:26 -0500

    Added Functionality to Comments to handle: //Likes and Dislikes //Planned Form Submission //Retrieve all Comments for filtering

---
+++ youtubeclone_frontend/src/components/Comments/Comments.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Reply from '../Reply/Reply';
+import CommentsDisplay from '../CommentsDisplay/CommentsDisplay';
+
+
+class Comments extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: [],
+            videoId: '',
+            comment: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getComments()
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+   }
+
+   getComments = async() => {
+       let response = await axios.get('http://127.0.0.1:8000/comment/')
+       let comments = response.data
+       let filteredComments = []
+       comments.forEach(comment => {
+           if(comment.videoId === this.props.videoId) {
+               filteredComments.push(comment)
+           }
+       })
+       this.setState({
+            videoId: this.props.videoId,
+            comments: filteredComments
+        })
+
+   }
+
+   handleSubmit = (event) => {
+       event.preventDefault();
+       let comment = {
+          comment: this.state.comment,
+          likes: 0,
+          dislikes: 0,
+          video: this.props.videoId 
+       }
+       this.setState({
+           comments: [...this.state.comments, comment]
+       })
+       await axios.post("http://127.0.0.1:8000/comment/", comment)
+       this.props.addComment()
+   }
+
+   increment = async(id, name) => {
+    let newArr = this.state.comments
+    let value;
+    newArr.forEach(comment => {
+        if(comment.id === id) {
+            comment[name] += 1
+            value = comment[name]
+        }
+    })
+    this.setState({
+        comments: newArr
+    })
+    await axios.patch(`http://127.0.0.1:8000/comments/${id}/`, { [name]: value })
+    }
+
+
+   render() {
+       return (
+         <div>
+
+         </div>
+        );
+   }
+
+
+
+
+}
