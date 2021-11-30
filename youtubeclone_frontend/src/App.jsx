0f8b9ab806d353946e389ec6afb6c21aa9eb9742
Howie21 <bryce9643@gmail.com>
2021-11-30 15:22:10 -0500

    Added: // getVideo to retreive a Default Video upon start of the Application. // getSearch to retrieve videos when a Search is triggered

--- youtubeclone_frontend/src/App.jsx
+++ youtubeclone_frontend/src/App.jsx
@@ -17,10 +17,30 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.getVideo()
 
   }
 
+  async getVideo(){
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=WuBUTa6cS5w&key=${this.state.apiKey}&part=snippet,contentDetails,statistics,status`)
+    this.setState({
+      videoId: response.data.items[0].id,
+      video: response.data.items[0]
+    })
+  }
+
+  getSearch = async(event) => {
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${this.state.apiKey}&q=${search}`)
+    this.setState({
+      videos: response.data.items,
+      videoId: response.data.items[0].id.videoId,
+      video: response.data.items[0]
+    })
+    this.getComments()
+  }
+
   
+
 
 
   render() { 
